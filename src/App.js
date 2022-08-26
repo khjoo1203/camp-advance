0970735390474c024b0fa72642c050084abd2376
dan-studio <danstudiocode@gmail.com>
2022-08-26 17:42:40 +0900

    router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Main from './page/Main';
 import Detail from './page/Detail'
-
+import { Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Main/>
-      <Detail/>
+      <Routes>
+      <Route path='/' element={<Main/>}/>
+      <Route path='/detail/:id' element={<Detail/>}/>
+      </Routes>
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,21 +2,26 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const Form = (props) => {
-    const [toggle, setToggle] = useState(false);
-   return (
+  const [toggle, setToggle] = useState(false);
+  return (
     <Formed>
-        <div>
-            <InputBox type="text" placeholder="Artist" />
-            <InputBox type="text" placeholder="title" />
-            <Button>Submit</Button>
-        </div>
-    
-        <div>
-        </div>
-            <ToggleButton onClick={() => {setToggle(!toggle)}} >-</ToggleButton>
+      <div>
+        <InputBox type="text" placeholder="Artist" />
+        <InputBox type="text" placeholder="title" />
+        <Button>Submit</Button>
+      </div>
+
+      <div></div>
+      <ToggleButton
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        -
+      </ToggleButton>
     </Formed>
-   ); 
-}
+  );
+};
 
 export default Form;
 
@@ -28,7 +33,7 @@ const Formed = styled.form`
 `;
 
 const InputBox = styled.input`
-  padding:8px 10px;
+  padding: 8px 10px;
   background-color: #764abc;
   line-height: 20px;
   font-size: 16px;
@@ -36,13 +41,15 @@ const InputBox = styled.input`
   border-radius: 20px;
   border: none;
   outline: none;
-  &::placeholder {color:#ccc;}
+  &::placeholder {
+    color: #ccc;
+  }
 `;
 
 const Button = styled.button`
   background-color: #764abc;
   padding: 10px;
-  color:#fff;
+  color: #fff;
   border-radius: 20px;
   border: none;
 `;
@@ -50,9 +57,9 @@ const Button = styled.button`
 const ToggleButton = styled.button`
   width: 50px;
   height: 30px;
-  color:#fff;
+  color: #fff;
   border-radius: 30px;
   border: none;
   cursor: pointer;
-  background-color: #764abc; 
+  background-color: #764abc;
 `;
