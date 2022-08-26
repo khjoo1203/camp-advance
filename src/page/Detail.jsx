5c4d2b48a2c253d7e73c5b4a944d8bd5b172753d
dan-studio <danstudiocode@gmail.com>
2022-08-26 17:22:38 +0900

    conflict solved

--- src/page/Detail.jsx
+++ src/page/Detail.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Info from '../components/Info'
 import CommentForm from '../components/CommentForm';
 import CommentList from '../components/CommentList';
 
 const Detail = () => {
   return (
     <div>
+      <Info/>
       <CommentList />
       <CommentForm />
     </div>
