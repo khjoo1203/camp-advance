3fddf30220c2119c140532d3b3cc90af87f62dcf
khjoo1203 <khjoo1203@naver.com>
2022-08-26 15:40:40 +0900

    form3

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -25,11 +25,13 @@ export default Form;
 const Formed = styled.form`
   max-width: 1200px;
   min-width: 800px;
+  background-color: #ccc;
+  margin: 0 auto;
 `;
 
 const InputBox = styled.input`
   padding:8px 10px;
-  background-color: #a055ff;
+  background-color: #764abc;
   line-height: 20px;
   font-size: 16px;
   color: #fff;
@@ -40,7 +42,7 @@ const InputBox = styled.input`
 `;
 
 const Button = styled.button`
-  background-color: #7100ff;
+  background-color: #764abc;
   padding: 10px;
   color:#fff;
   border-radius: 20px;
@@ -48,10 +50,11 @@ const Button = styled.button`
 `;
 
 const ToggleButton = styled.button`
-  width: 130px;
+  width: 50px;
   height: 30px;
+  color:#fff;
   border-radius: 30px;
   border: none;
   cursor: pointer;
-  background-color: #7100ff; 
+  background-color: #764abc; 
 `;
