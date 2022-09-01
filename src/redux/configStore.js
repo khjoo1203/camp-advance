1bf9a3c4cab9ef224799469fb7ae9d5a0be870bb
khjoo1203 <khjoo1203@naver.com>
2022-09-01 15:59:59 +0900

    커밋

---
+++ src/redux/configStore.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from '@reduxjs/toolkit';
+import musicReducer from './module/musicSlice'
+import commentReducer from './module/commentSlice'
+
+const rootReducer = combineReducers({
+  musics: musicReducer,
+  comments: commentReducer
+})
+
+const store = configureStore({
+  reducer: rootReducer
+})
+
+export default store;
