b460262f6d81adae22ed2127500b00e76932d71d
dan-studio <danstudiocode@gmail.com>
2022-09-01 14:00:23 +0900

    error fixed

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const URL = "http://localhost:3001/list";
+const URL = "https://duxmusic.herokuapp.com/list";
 const instance = axios.create({ baseURL: URL });
 export const getItems = async (page, limit) => {
   const response = await instance.get(`/?_page=${page}&_limit=${limit}`);
